0d478cf9e48a6e1cb1d8fcb0eb806e89521321c0
@JuliusDev <raagasjulius34@gmail.com>
2023-03-08 17:50:17 +0800

    changing base url

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/",
+      path: "/recipe-app-project",
       name: "search-page",
       component: SearchView,
     },
