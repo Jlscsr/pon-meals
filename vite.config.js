2bbcc34ce8f65991273959ca9956087918ccd6a5
@JuliusDev <raagasjulius34@gmail.com>
2023-03-24 00:35:47 +0800

    Made small changes

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/pon-meals/", // for github pages
-  plugins: [vue()],
-});
